61b323f761acdfeacb98cad4f4015c802bbc1a7d
Hieu Nghia Vuong <wechicken456@Hieus-MBP.modem>
2021-07-06 19:06:59 +1000

    Added new solutions

--- graph/building_roads.cpp
+++ graph/building_roads.cpp
@@ -29,15 +29,14 @@ ll my_ceil(ll a, ll b) {
 }
 
 const int maxN = 1e5;
-const int maxM = 2e5;
+const int maxX = 1000;
 int n, m,x;
 bool visited[maxN + 1];
-vector<int>adj[maxN + 1];
+vector<int>adj[maxN + 1], ans;
 
 void dfs(int cur){
 	if (visited[cur]) return;
 	visited[cur] = true;
-	
 	// process node cur
 	for (auto &c : adj[cur]) {
 		dfs(c);
@@ -46,18 +45,25 @@ void dfs(int cur){
 
 void solve(){
 	cin >> n >> m;
-	int a,b;
-	for (int i = 0 ; i < m; i++){
+	for (int i = 1, a, b ; i <= m; i++){
 		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
+		adj[a].pb(b);
+		adj[b].pb(a);
 	}
-	
+	for (int i = 1 ; i <= n; i++){		
+		if (!visited[i]) {				// if a node is not visited, that means there's no path from the previous node to it, so we need to connect these nodes. => ans increment. 
+			dfs(i);
+			ans.pb(i);					// does not matter which cities we connect, we can still get to other cities as long as there's a path.
+		}
+	}
+	cout << ans.size() - 1 << "\n";		// if ans = 1 that means there's a path to every city from any starting point.
+	for (int i = 1; i < ans.size(); i++){
+		cout << ans[0] << " "  << ans[i] << "\n";
+	}
 }
 
 int main(){
 	solve();
 }
-
 
 
